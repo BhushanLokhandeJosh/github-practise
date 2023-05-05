b274b9a2b31fd59fcb3b568826eb73089a445047
BhushanLokhandeJosh <118346485+BhushanLokhandeJosh@users.noreply.github.com>
2023-05-05 13:38:40 +0530

    usesuccess props removed

--- src/hooks/useQueryhooks.ts
+++ src/hooks/useQueryhooks.ts
@@ -1,12 +1,6 @@
 import { useMutation } from "react-query";
 import { createConfiguration } from "../services/config-services";
 
-
-interface IProps {
-  onSuccess:(values:any) => void;
-  onError:(values:any) => void
-}
-
 export const useCreateConfiguration = (props: IProps) => {
   const { onSuccess, onError } = props;
   return useMutation(createConfiguration, {
